b21b5d2ec98ef3d4e64e1c40cbbd3c7f3d9ea4e6
l <l@l.hsd1.nj.comcast.net>
2023-03-12 23:15:26 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from coinsapi import *
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+app.secret_key = ""
+
+load_api()
+
+coins_data = create_coin('all')
+
+@app.route("/")
+def main():
+    return render_template("index.html", coins_data = coins_data)
+
+@app.route("/<coin>")
+def coin_page(coin):
+    coin_info = create_coin(coin)
+    return render_template("coinpage.html", coin_info = coin_info)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
